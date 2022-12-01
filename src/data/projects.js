017ddb04cca3584cb11267af1446ce0ac2dbc68a
trent_rush <trent_rush@papajohns.com>
2022-12-01 13:45:33 -0600

    update projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,11 +1,11 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: '',
-    subtitle: '',
+    title: 'Sorry, No projects yet',
+    subtitle: 'It is because the site is still new, but do not worry cause there are some things on the way.',
     link: '',
     image: '',
-    date: '2022-11-17',
+    date: '2022-12-1',
     desc: '',
   },
 ];
